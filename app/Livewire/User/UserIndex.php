b1e68fa8334db3c37ba86736ecbe4dbef11d2500
Hafid <hafid@gmail.com>
2025-04-03 21:40:45 +0700

    update:deletefor account

--- app/Livewire/User/UserIndex.php
+++ app/Livewire/User/UserIndex.php
@@ -14,6 +14,15 @@ class UserIndex extends Component
     {
         $this->user = User::all();
     }
+
+    public function deleteUser($id)
+    {
+        $user = User::findOrFail($id);
+        $user->delete();
+        session()->flash('success', 'Data berhasil dihapus.');
+
+        $this->redirectRoute('user.index');
+    }
     public function render()
     {
         return view('livewire.user.user-index');
